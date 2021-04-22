735ae874961eda60228da8de08532e4904184816
dRyaba <79122925+dRyaba@users.noreply.github.com>
2021-04-22 18:50:48 +0300

    Update main.c
    deleted mallocs

--- main.c
+++ main.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 typedef struct matrix {
-    double **matrix;
-    double det;
+    float matrix[64][64];
+    float det;
     int size;
 } Matrix;
 
-
-double determinant(double **a, int n) {
-    if (n == 1)
-        return a[0][0];
-    else if (n == 2)
-        return a[0][0] * a[1][1] - a[0][1] * a[1][0];
+float determinent(float matrix[64][64], int size) {
+    int m, n;
+    if (size == 1) {
+        return (matrix[0][0]);
+    } else if (size == 2)
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     else {
-        double d = 0;
-        for (int k = 0; k < n; k++) {
-            double **m = (double **) malloc(sizeof(double *) * n);
-            if (*m) {
-                for (int i = 0; i < n - 1; i++)
-                    m[i] = (double *) malloc(sizeof(double) * n);
-
-                for (int i = 1; i < n; i++) {
-                    if (m[i - 1]) {
-                        int t = 0;
-                        for (int j = 0; j < n; j++) {
-                            if (j == k)
-                                continue;
-                            m[i - 1][t] = a[i][j];
-                            t++;
+        float det = 0, s = 1, b[64][64];
+        for (int c = 0; c < size; c++) {
+            m = n = 0;
+            for (register int i = 0; i < size; i++) {
+                for (register int j = 0; j < size; j++) {
+                    b[i][j] = 0;
+                    if (i != 0 && j != c) {
+                        b[m][n] = matrix[i][j];
+                        if (n < (size - 2)) {
+                            n++;
+                        } else {
+                            n = 0;
+                            m++;
                         }
                     }
                 }
-                d += pow(-1, k + 2) * a[0][k] * determinant(m, n - 1);
-                for (int i = 0; i < n - 1; i++)
-                    free(m[i]);
-                free(*m);
             }
+            det += s * (matrix[0][c] * determinent(b, size - 1));
+            s = -1 * s;
         }
-        return d;
+    return (det);
     }
 }
-
 
 void qs(Matrix *arr, int first, int last) {
     if (first < last) {
@@ -64,40 +57,32 @@ void qs(Matrix *arr, int first, int last) {
     }
 }
 
-void print_matrix(double **arr, int k) {
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < k; j++) {
-            printf("%d ", (int) arr[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 int main() {
     int n, k;
-//    freopen("input.txt", "r", stdin);
-    scanf("%d", &n);
-    Matrix *arr = (Matrix *) malloc(sizeof(Matrix) * n);
-    if (arr) {
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &k);
-            arr[i].matrix = (double **) malloc(sizeof(double *) * k);
-            if (arr[i].matrix) {
-                for (int j = 0; j < k; j++) {
-                    arr[i].matrix[j] = (double *) malloc(sizeof(double) * k);
-                    if (arr[i].matrix[j]) {
-                        for (int t = 0; t < k; t++)
-                            scanf("%lf", &arr[i].matrix[j][t]);
-                    }
-                }
-                arr[i].det = determinant(arr[i].matrix, k);
-                arr[i].size = k;
-            }
+    FILE *input, *output;
+    input = fopen("input.txt", "r"), output = fopen("output.txt", "w");
+    if ((input == NULL) || (output == NULL))
+        return 72;
+    fscanf(input, "%d", &n);
+    Matrix arr[n];
+    for (int i = 0; i < n; i++) {
+        fscanf(input, "%d", &k);
+        for (int j = 0; j < k; j++) {
+            for (int t = 0; t < k; t++)
+                fscanf(input, "%f", &arr[i].matrix[j][t]);
         }
-        qs(arr, 0, n - 1);
-        for (int i = 0; i < n; i++)
-            print_matrix(arr[i].matrix, arr[i].size);
+        arr[i].det = determinent(arr[i].matrix, k);
+        arr[i].size = k;
     }
-
-    free(arr);
+    fclose(input);
+    qs(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < arr[i].size; j++) {
+            for (int t = 0; t < arr[i].size; t++) {
+                fprintf(output, "%d ", (int) arr[i].matrix[j][t]);
+            }
+            fprintf(output, "\n");
+        }
+    }
+    fclose(output);
 }
